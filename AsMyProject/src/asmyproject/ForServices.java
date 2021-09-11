a46ef7da15ebc5f852199e37a4c087ab93032f6a
faxavier <irankfx@gmail.com>
2021-09-11 19:44:55 +0200

    AsMyProject commit

---
+++ AsMyProject/src/asmyproject/ForServices.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asmyproject;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Xavier de Jesus
+ */
+public class ForServices {
+    void admin(){
+        String admin="xavier",pass="paccy";
+        JOptionPane.showConfirmDialog(null, "login first");
+        
+        
+        
+    }
+    public static void main(String args[]){
+        
+    }
+    
+}
